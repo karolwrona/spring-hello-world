878b519724d65f54d8054f6cce5028dad4245a61
kwronski <karol.mikolaj.wronski@gmail.com>
2019-02-09 14:50:58 +0100

    09.02.2019 14:50

--- src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
+++ src/main/java/pl/edu/wszib/springhelloworld/configuration/MainConfiguration.java
@@ -2,10 +2,14 @@ package pl.edu.wszib.springhelloworld.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import pl.edu.wszib.springhelloworld.model.EkspresDoKawy;
 import pl.edu.wszib.springhelloworld.model.Komputer;
 import pl.edu.wszib.springhelloworld.model.Kubek;
 import pl.edu.wszib.springhelloworld.model.Programista;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SINGLETON;
 
 @Configuration
 public class MainConfiguration {
@@ -23,6 +27,24 @@ public class MainConfiguration {
     @Bean
     public Programista lukasz(Kubek kubek, Komputer komputer, EkspresDoKawy ekspresDoKawy) {
         return new Programista(kubek, komputer, ekspresDoKawy);
+    }
+
+    @Bean
+    @Scope(SCOPE_PROTOTYPE)
+    public Komputer kumputer() {
+        return new Komputer();
+    }
+
+    @Bean
+    @Scope(SCOPE_SINGLETON)
+    public EkspresDoKawy ekspresDoKawy() {
+        return new EkspresDoKawy();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Kubek kubek() {
+        return new Kubek();
     }
 
 }

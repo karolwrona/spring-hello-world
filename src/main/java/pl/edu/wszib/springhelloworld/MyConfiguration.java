84075f1e5739622d70260e95cdcb5926bd1ddca8
kwronski <karol.mikolaj.wronski@gmail.com>
2019-02-09 11:28:22 +0100

    09.02.2019 11:28

--- src/main/java/pl/edu/wszib/springhelloworld/MyConfiguration.java
+++ src/main/java/pl/edu/wszib/springhelloworld/MyConfiguration.java
@@ -1,19 +1,30 @@
 package pl.edu.wszib.springhelloworld;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import pl.edu.wszib.springhelloworld.model.Kot;
 import pl.edu.wszib.springhelloworld.model.Tygrys;
 
 @Configuration
 public class MyConfiguration {
+    @Bean ("burek")
+    public Kot kot1() {
+        return new Kot();
+    }
+
     @Bean
-    public Tygrys tygrys() {
-        return new Tygrys();
+    @Primary //jak nie wskazany inny to zawsze on!
+    public Kot kot2() {
+        return new Kot();
     }
     @Bean
-    public String execute(Kot kot) {
-        kot.glos();
-        return"";
+    public Kot kot3() {
+        return new Kot();
+    }
+
+    @Bean String execute(@Qualifier("burek") Kot dsfsdf) {
+        return "";
     }
 }
